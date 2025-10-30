9a8d36e4809811244bba1b6f15480a5688c4d0b0
alamdilshad87 <dilshadand@gmail.com>
2025-10-30 13:00:21 +0530

    Clean

--- SampleCode/SampleCode/obj/Release/net8.0/SampleCode.AssemblyInfo.cs
+++ SampleCode/SampleCode/obj/Release/net8.0/SampleCode.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleCode")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eafdd77520fba6c8b1e2ffcec3d249ebcd66cdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleCode")]
