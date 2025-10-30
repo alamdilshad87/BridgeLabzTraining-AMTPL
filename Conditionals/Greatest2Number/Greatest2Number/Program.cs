a8302597b34a9e482f6b5375e24ac48ada656417
alamdilshad87 <dilshadand@gmail.com>
2025-10-30 14:56:20 +0530

    Greatest of 2 Number

---
+++ Conditionals/Greatest2Number/Greatest2Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Greatest2Number
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter first number:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        if(num1> num2)
+        {
+            Console.WriteLine("The greatest number is: " + num1);
+        }
+        else
+        {
+            Console.WriteLine("The greatest number is: " + num2);
+        }
+    }
+}
