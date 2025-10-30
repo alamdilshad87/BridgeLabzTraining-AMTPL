1ba735fde0bbe51dc1bd614c555f6d1be9dffd5c
alamdilshad87 <dilshadand@gmail.com>
2025-10-30 15:43:23 +0530

    Flip Coin

---
+++ Looping/FlipCoin/FlipCoin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class FlipCoin
+    {
+    static void Main()
+    {
+        int flips;
+        Console.WriteLine("Enter number of times to flip the coin: ");
+        while (!int.TryParse(Console.ReadLine(), out flips) || flips <= 0)
+        {
+            Console.Write("Please enter a positive integer: ");
+        }
+
+        int heads = 0, tails = 0;
+        Random random = new Random();
+
+        for (int i = 0; i < flips; i++)
+        {
+            double result = random.NextDouble();
+            if (result < 0.5)
+                tails++;
+            else
+                heads++;
+        }
+
+        double headPercentage = (heads * 100.0) / flips;
+        double tailPercentage = (tails * 100.0) / flips;
+
+        Console.WriteLine("\nResults after " + flips + " flips:");
+        Console.WriteLine("Heads: " + headPercentage + "%");
+        Console.WriteLine("Tails: " + tailPercentage + "%");
+    }
+}
