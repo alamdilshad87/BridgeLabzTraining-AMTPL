d3c2532853e445dfeb90c1bb105af5db6a7a8b76
alamdilshad87 <dilshadand@gmail.com>
2025-10-30 16:03:35 +0530

    Flip Coin, Top10AvgSum and Astersik Pattern Solved

---
+++ Looping/Top10AvgSum/Top10AvgSum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Top10AvgSum
+{
+    static void Main()
+    {
+        int sum = 0, avg = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            sum += i;
+        }
+
+        double average = (double)sum / 10;
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average:F2}");
+    }
+}
