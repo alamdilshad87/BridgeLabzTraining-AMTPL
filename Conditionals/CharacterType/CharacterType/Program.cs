59dccec37c38aafda5e4859a02f1277d44ff057c
alamdilshad87 <dilshadand@gmail.com>
2025-10-30 15:37:13 +0530

    CharacterType & Leap Year

---
+++ Conditionals/CharacterType/CharacterType/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class CharacterType
+{
+    static void Main()
+    {
+        Console.Write("Enter a character: ");
+        char character = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (char.IsLetter(character))
+        {
+            Console.WriteLine($"{character} is a letter.");
+        }
+        else if (char.IsDigit(character))
+        {
+            Console.WriteLine($"{character} is a digit.");
+        }
+        else if (char.IsWhiteSpace(character))
+        {
+            Console.WriteLine($"{character} is a whitespace character.");
+        }
+        else
+        {
+            Console.WriteLine($"{character} is a special character.");
+        }
+    }
+}
---
+++ Conditionals/LeapYear/LeapYear/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year:");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine($"{year} is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a leap year.");
+            }
+        }
+    }
+}
